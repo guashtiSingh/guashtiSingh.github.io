5d1a196f094aba1cd886269f7bbc92c14454c389
guashtiSingh <gsingh540@my.centennialcollege.ca>
2015-07-10 15:41:32 -0400

    Assignment#3-WebGameScroller

---
+++ COMP397MailPilot/Scripts/objects/label.ts
@@ -0,0 +1,30 @@
+﻿module objects {
+    // LABEL CLASS
+    export class Label extends createjs.BitmapText {
+        // INSTANCE VARIALBES
+        public width: number;
+        public height: number;
+
+        // CONSTRUCTOR +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        constructor(labelString: string, x: number, y: number) {
+            super(labelString, assets.fontAtlas);
+
+            this.setSize(40); // set default font size to 40 px;
+
+            this.width = this.getBounds().width;
+            this.height = this.getBounds().height;
+
+            this.regX = this.width * 0.5;
+            this.regY = this.height * 0.5;
+
+            this.x = x;
+            this.y = y;
+        }
+
+        setSize(newSize: number) {
+            var fontSize: number = newSize / 100;
+            this.scaleX = fontSize;
+            this.scaleY = fontSize;
+        }
+    }
+}
---
+++ COMP397MailPilot/Scripts/states/menu.ts
@@ -0,0 +1,63 @@
+﻿
+/// <reference path="../objects/gameobject.ts" />
+/// <reference path="../objects/cat.ts" />
+/// <reference path="../objects/cheese.ts" />
+/// <reference path="../objects/wood.ts" />
+/// <reference path="../objects/mouse.ts" />
+/// <reference path="../objects/button.ts" />
+/// <reference path="../objects/label.ts" />
+/// <reference path="../objects/scoreboard.ts" />
+
+
+module states {
+    // MENU STATE
+    export class Menu {
+        // INSTANCE VARIABLES ++++++++++++++++++++++++++++++++++++++++++++++
+        public game: createjs.Container;
+        public wood: objects.Wood;
+        public playButton: objects.Button;
+
+        // CONSTRUCTOR ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        constructor() {
+            // Instantiate Game Container
+            this.game = new createjs.Container();
+
+            // Add wood(background) to game
+            this.wood = new objects.Wood();
+            this.game.addChild(this.wood);
+
+            //The Menu label
+            var gameScrollerLabel: objects.Label = new objects.Label("TOM AND JERRY", 320, 100);
+            gameScrollerLabel.setSize(60);
+            gameScrollerLabel.regX = gameScrollerLabel.getBounds().width * 0.5;
+            gameScrollerLabel.regY = gameScrollerLabel.getBounds().height * 0.5;
+            this.game.addChild(gameScrollerLabel);
+
+            //Add play button to the game screen
+            this.playButton = new objects.Button("playButton", 320, 400);
+            this.game.addChild(this.playButton);
+            this.playButton.on("click", this.playButtonClicked, this);
+
+
+            stage.addChild(this.game);
+        } // constructor end
+
+
+        // PUBLIC METHODS ++++++++++++++++++++++++++++++++++++++++++++++++++++
+        playButtonClicked() {
+            this.game.removeAllChildren();
+            stage.removeChild(this.game);
+            currentState = 1;
+            stateChanged = true;
+        }
+
+        // UPDATE METHOD
+        public update() {
+
+            this.wood.update();
+
+        } // update method end
+
+
+    }
+} 
